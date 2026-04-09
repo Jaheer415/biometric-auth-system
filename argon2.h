5892ed27217f80e5bd4b4600193b3c3c91aa85d3
Jaheer415 <jaheer973131@gmail.com>
2026-04-09 14:32:02 +0530

    Delete argon2.h

--- argon2.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARGON2_WRAPPER_H
-#define ARGON2_WRAPPER_H
-
-#include <string>
-
-std::string argon2_hash_string(const std::string& password, const std::string& salt);
-std::string generateSalt(int length);
-
-#endif
